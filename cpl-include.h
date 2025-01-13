3c0f1521821fa2b76a4c9f9551cde26a836d7d03
Bill Jojo <jojowil@gmail.com>
2025-01-12 22:53:32 -0500

    use one include file. this might be a mistake, but it feels good right now.

--- cpl-include.h
+++ cpl-include.h
@@ -11,9 +11,12 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <stddef.h>
 
+#include "git-banned.h"
 #include "cpl-enums.h"
 #include "cpl-strings.h"
 #include "cpl-util.h"
+#include "cpl-functions.h"
 
 #endif //CPL_CPL_INCLUDE_H
