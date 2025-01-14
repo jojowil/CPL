998b5f0f860437cbe9d06c19826b7cb5dd3bca83
Bill Jojo <jojowil@gmail.com>
2025-01-13 21:02:23 -0500

    updated header files

---
+++ cpl-functions.h
@@ -0,0 +1,8 @@
+//
+// Created by Bill Jojo on 1/12/25.
+//
+
+#ifndef CPL_CPL_FUNCTIONS_H
+#define CPL_CPL_FUNCTIONS_H
+
+#endif //CPL_CPL_FUNCTIONS_H
--- cpl-strings.h
+++ cpl-strings.h
@@ -5,6 +5,7 @@
 #ifndef CPL_CPL_STRINGS_H
 #define CPL_CPL_STRINGS_H
 
-long cpl_strtol(const char*);
+long cpl_strtol(const char*, int);
+size_t cpl_indexOf(const char *, const char *);
 
 #endif //CPL_CPL_STRINGS_H
