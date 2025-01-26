420af023c1dd5700a0089b0a42d21430483f1070
Bill Jojo <jojowil@gmail.com>
2025-01-26 16:19:59 -0500

    more variable work.

--- cpl-vars.c
+++ cpl-vars.c
@@ -1,16 +1,74 @@
-#include <stddef.h>
-#include <stdlib.h>
+#include "cpl-include.h"
+
+/* This is a simple linked list lib for managing variables. */
 
 typedef enum {
-    INT
+    INT, CHAR, CHARL, DATE
 } vartype;
 
-typedef struct variable {
-    char name[32];
+#define VAR_NAME_LEN 33 // 32 chars plus null
+
+struct variable {
+    char name[VAR_NAME_LEN];
     vartype type;
     char *val;
-    variable *next;
-} variable_node;
+    struct variable *next;
+};
 
+struct variable *cpl_variables = NULL;
 
-#include "cpl-vars.h"
+// find a var pointer. O(n)
+struct variable *cpl_find_var(const char *name) {
+    // upcase var name
+    char n[VAR_NAME_LEN];
+    snprintf(n, VAR_NAME_LEN, "%s", name);
+    strtoupper(n);
+
+    struct variable *p = cpl_variables;
+    while (p != NULL) {
+        if (strcmp(p->name, n) == 0)
+            return p;
+        p = p->next;
+    }
+    return NULL;
+}
+
+void cpl_set_var(const char *name, const char *val) {
+    // get node space
+    struct variable *n;
+    // get existing or create new
+    if ((n = cpl_find_var(name)) == NULL)
+        n = malloc(sizeof(struct variable));
+    else
+        free(n->val); // get rid of old val!
+    if (n == NULL)
+        cpl_error_end(17, 0, name);
+    // get var val space. name len limited!
+    size_t len = strlen(val) + 1;
+    n->val = malloc(len);
+    if (n->val == NULL)
+        cpl_error_end(17, 0, name);
+    //set var name
+    snprintf(n->name, VAR_NAME_LEN, "%s", strtoupper(name));
+    snprintf(n->val, len, "%s", val);
+    // add to list. O(1)
+    n->next = cpl_variables;
+    cpl_variables = n;
+}
+
+// get var value. O(n)
+char *cpl_get_var(const char *name) {
+    struct variable *n = cpl_find_var(name);
+    return (n != NULL) ? n->val : NULL;
+}
+
+// destroy all variable data.
+void cpl_destroy_all_vars() {
+    struct variable *c, *p = cpl_variables;
+    while (p != NULL) {
+        free(p->val);
+        c = p;
+        p = p->next;
+        free(c);
+    }
+}
--- cpl-vars.h
+++ cpl-vars.h
@@ -1,4 +1,12 @@
 #ifndef CPL_CPL_VARS_H
 #define CPL_CPL_VARS_H
 
+struct variable *cpl_find_var(const char *name);
+
+void cpl_add_var(const char *name, const char *val);
+
+char *cpl_get_var(const char *name);
+
+void cpl_destroy_all_vars();
+
 #endif //CPL_CPL_VARS_H
